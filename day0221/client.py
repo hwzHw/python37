811b9e7d3ba38ac8640a78e83236687da5147ec3
Your Name <544306770@qq.com>
2019-02-21 08:41:50 +0800

    day0221 客服端服务端交流

---
+++ day0221/client.py
@@ -0,0 +1,39 @@
+import threading
+import socket
+# 客服端
+server = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+server.bind(("192.168.15.6", 60000))
+addr = None
+
+
+def read():
+    while True:
+        try:
+
+            result = server.recvfrom(1024)
+            print(result)
+            global addr
+            addr = result[1]
+        except:
+            print("end")
+            break
+
+
+def write():
+    while True:
+        try:
+            info = input("输入发送内容：")
+            if info.__eq__("exit"):
+                server.close()
+                break
+            else:
+                server.sendto(info.encode("utf8"), addr)
+        except:
+            print("end")
+            break
+
+
+t1 = threading.Thread(target=read)
+t2 = threading.Thread(target=write)
+t1.start()
+t2.start()
---
+++ day0221/server.py
@@ -0,0 +1,35 @@
+import threading
+import socket
+
+# 服务端
+client = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
+
+
+def read():
+    client.sendto("".encode("utf8"), ("192.168.15.6", 60000))
+    while True:
+        try:
+            result = client.recvfrom(1024)
+            print(result[0].decode("utf8"))
+        except :
+            print("end")
+            break
+
+
+def write():
+    while True:
+        str1 = input("输入内容：")
+        if str1.__eq__("exit"):
+            client.close()
+            print(client)
+
+            # client = None
+            break
+        else:
+            client.sendto(str1.encode("utf8"), ("192.168.15.6", 60000))
+
+
+tread = threading.Thread(target=read)
+twrite = threading.Thread(target=write)
+tread.start()
+twrite.start()
